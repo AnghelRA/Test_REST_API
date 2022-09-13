ee9a7b0c97e88113db80053f28482e08c248fbff
DetectiveRa <angelr.a99@tutanota.com>
2022-09-13 12:26:20 +0300

    Update test_rest_api.py

--- test_rest_api.py
+++ test_rest_api.py
@@ -1,48 +1,104 @@
 from pyats import aetest
-import json
-from utils import RestUtils
+from utils import Users, Posts, Comments, Todos
 
-class TestRest(aetest.Testcase):
-    @aetest.setup
+
+class ScriptCommonSetup(aetest.CommonSetup):
+
+    @aetest.subsection
     def setup(self):
-        rest_util_obj = RestUtils()
-        self.parent.parameters['rest_utils_obj'] = rest_util_obj
-        self.json_data = json.load(open('user.json', 'r'))
-        self.page = "users"
+        self.parent.parameters['users'] = Users()
+        self.parent.parameters['posts'] = Posts()
+        self.parent.parameters['comments'] = Comments()
+        self.parent.parameters['todos'] = Todos()
 
-    @aetest.test
-    def test_fetch(self, rest_utils_obj):
-        result = rest_utils_obj.fetch_information("users")
-        if result.status_code != 200:
+    @aetest.subsection
+    def test_connections(self, users, posts, comments, todos):
+        result1 = users.fetch_users()
+        result2 = posts.fetch_posts()
+        result3 = comments.fetch_comments()
+        result4 = todos.fetch_todos()
+
+        if result1.status_code != 200 or result2.status_code != 200 \
+                or result3.status_code != 200 or result4.status_code != 200:
             self.failed()
 
+
+class TestCreateUser(aetest.Testcase):
+
+    must_pass = True
+
     @aetest.test
-    def test_create_object(self, rest_utils_obj):
-        result = rest_utils_obj.create_object(self.page, self.json_data)
-        if result.status_code != 201:
+    def test_create_user(self, users):
+        result = users.create_user()
+        if not result:
             self.failed()
 
+        self.parent.parameters['user_id'] = result
+
+class TestCreatePost(aetest.Testcase):
+
+    must_pass = True
+
     @aetest.test
-    def test_object_created(self, rest_utils_obj):
-        self.list_id = rest_utils_obj.check_created_object(self.page, self.json_data)
-        if not self.list_id:
+    def test_create_post(self, posts, user_id):
+        result = posts.create_post(user_id)
+
+        if not result:
             self.failed()
 
+        self.parent.parameters['post_id'] = result
+
+class TestCreateComment(aetest.Testcase):
+
+    must_pass = True
+
     @aetest.test
-    def test_delete_object(self, rest_utils_obj):
-        self.id = self.list_id[0]
-        result = rest_utils_obj.delete_object(self.page, self.id)
-        if result.status_code != 204:
-           self.failed(result.status_code)
+    def test_create_post(self, comments, post_id):
+        result = comments.create_comment(post_id)
 
-    @aetest.cleanup
-    def cleanup(self, rest_utils_obj):
-        if not self.list_id:
-            for id in self.list_id:
-                rest_utils_obj.delete_object(self.page, id)
-        del self.id
-        del self.json_data
+        if not result:
+            self.failed()
 
+        self.parent.parameters['comment_id'] = result
+
+class TodoCreateComment(aetest.Testcase):
+
+    must_pass = True
+
+    @aetest.test
+    def test_create_post(self, todos, user_id):
+        result = todos.create_todo(user_id)
+
+        if not result:
+            self.failed()
+
+        self.parent.parameters['todo_id'] = result
+
+class ScriptCommonCleanup(aetest.CommonCleanup):
+
+    @aetest.subsection
+    def delete_users(self, users, user_id):
+        result = users.delete_user(user_id)
+        if not result:
+            self.failed(result)
+
+    @aetest.subsection
+    def delete_posts(self, posts, post_id):
+        result = posts.delete_post(post_id)
+        if not result:
+            self.failed(result)
+
+    @aetest.subsection
+    def delete_comments(self, comments, comment_id):
+        result = comments.delete_comment(comment_id)
+        if not result:
+            self.failed(result)
+
+    @aetest.subsection
+    def delete_todos(self, todos, todo_id):
+        result = todos.delete_todo(todo_id)
+        if not result:
+            self.failed(result)
 
 if __name__ == '__main__':
     aetest.main()
