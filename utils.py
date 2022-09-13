425eb7ed7f164cd64e97c1b938b8549b76f7cd3d
DetectiveRa <angelr.a99@tutanota.com>
2022-09-13 12:28:10 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import requests
+import json
+
 
 class RestUtils:
     def __init__(self):
@@ -8,7 +10,6 @@ class RestUtils:
                         "Content-Type": "application/json",
                         "Authorization": "Bearer " + "f34a54cb0d65306812337c51922d6839bc60c50f1147a62c45f3732d49de7fbd"
                        }
-
 
     def fetch_information(self, page):
         response = requests.get(self.url + page, headers=self.headers, verify=False)
@@ -23,11 +24,100 @@ class RestUtils:
         return response
 
     def check_created_object(self, page, json_data):
-        list_id = []
         response = self.fetch_information(page)
         site_data = response.json()
         for data in site_data:
             if json_data.items() <= data.items():
                 id = data["id"]
-                list_id.append(str(id))
-        return list_id
+                return id
+
+
+class Users(RestUtils):
+
+    def __init__(self):
+        self.page = "users"
+        self.json_data = json.load(open('user.json', 'r'))
+        super().__init__()
+
+    def fetch_users(self):
+        return self.fetch_information(self.page)
+
+    def create_user(self):
+        self.create_object(self.page, self.json_data)
+        return self.check_created_object(self.page, self.json_data)
+
+    def delete_user(self, id):
+        try:
+            self.delete_object(self.page, str(id))
+        except:
+            return False
+        return True
+
+
+class Posts(RestUtils):
+
+    def __init__(self):
+        self.page = "posts"
+        self.json_data = json.load(open('post.json', 'r'))
+        super().__init__()
+
+    def fetch_posts(self):
+        return self.fetch_information(self.page)
+
+    def create_post(self, id):
+        self.json_data["user_id"] = id
+        self.create_object(self.page, self.json_data)
+        return self.check_created_object(self.page, self.json_data)
+
+    def delete_post(self, id):
+        try:
+            self.delete_object(self.page, str(id))
+        except:
+            return False
+        return True
+
+
+class Comments(RestUtils):
+
+    def __init__(self):
+        self.page = "comments"
+        self.json_data = json.load(open('comment.json', 'r'))
+        super().__init__()
+
+    def fetch_comments(self):
+        return self.fetch_information(self.page)
+
+    def create_comment(self, id):
+        self.json_data["post_id"] = id
+        self.create_object(self.page, self.json_data)
+        return self.check_created_object(self.page, self.json_data)
+
+    def delete_comment(self, id):
+        try:
+            self.delete_object(self.page, str(id))
+        except:
+            return False
+        return True
+
+
+class Todos(RestUtils):
+
+    def __init__(self):
+        self.page = "todos"
+        self.json_data = json.load(open('todo.json', 'r'))
+        super().__init__()
+
+    def fetch_todos(self):
+        return self.fetch_information(self.page)
+
+    def create_todo(self, id):
+        self.json_data["user_id"] = id
+        self.create_object(self.page, self.json_data)
+        return self.check_created_object(self.page, self.json_data)
+
+    def delete_todo(self, id):
+        try:
+            self.delete_object(self.page, str(id))
+        except:
+            return False
+        return True
